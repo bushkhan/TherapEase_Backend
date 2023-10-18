abe2f0900d612c9e1ea1055f0582aa6556ffeb02
Bushra Khan <bushrakhan2212002@gmail.com>
2023-10-18 14:44:59 +0530

    commit

--- accounts/emails.py
+++ accounts/emails.py
@@ -6,7 +6,13 @@ from .models import CustomUser
 def send_otp_via_email(email):
     subject = "Your account verification email"
     otp = random.randint(1000,9999)
-    message = f"Your otp is {otp}"
+    message = f"Hello,\n\n" \
+              f"Thank you for registering with our service. To verify your account, please use the following One-Time Password (OTP):\n\n" \
+              f"OTP: {otp}\n\n" \
+              f"This OTP is valid for a single use and will expire in 10 minutes. Please enter this OTP on the verification page to activate your account.\n\n" \
+              f"If you didn't request this OTP, please disregard this email and contact our support team immediately.\n\n" \
+              f"Best regards,\n" \
+              f"TherapEase Team"
     email_from = settings.EMAIL_HOST
     send_mail(subject,message,email_from, [email])
     user_obj = CustomUser.objects.get(email= email)
